c729f9bd20fb8738c0846935ce39bdb55c025e71
unknown <karindiaye3038@gmail.com>
2026-08-19 11:39:02 +0000

    fix render port

--- Backend/server.js
+++ Backend/server.js
@@ -138,7 +138,6 @@ app.get("/api/projects/archived", async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Serveur démarré sur http://localhost:${PORT}`);
- 
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
 });
