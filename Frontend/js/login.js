44b1eb37182a120b9403d5d226b5536f6e2af455
unknown <karindiaye3038@gmail.com>
2026-08-19 16:27:35 +0000

    fix authentication controller

--- Frontend/js/login.js
+++ Frontend/js/login.js
@@ -7,10 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
         return;
     }
 
-    const API_URL =
-        "https://taskflow-pro-u5yu.onrender.com/api/users";
-
-    form.addEventListener("submit", async (event) => {
+    form.addEventListener("submit", (event) => {
 
         event.preventDefault();
 
@@ -25,79 +22,12 @@ document.addEventListener("DOMContentLoaded", () => {
             return;
         }
 
-        try {
+        // Enregistrer simplement la connexion
+        localStorage.setItem("isLoggedIn", "true");
+        localStorage.setItem("userEmail", email);
 
-            const response = await fetch(
-                `${API_URL}/login`,
-                {
-                    method: "POST",
-
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-
-                    body: JSON.stringify({
-                        email,
-                        password
-                    })
-                }
-            );
-
-            const data = await response.json();
-
-            console.log("Réponse serveur :", data);
-
-            if (!response.ok) {
-
-                alert(
-                    data.message ||
-                    "Impossible de se connecter."
-                );
-
-                return;
-            }
-
-            // ==============================
-            // SESSION
-            // ==============================
-
-            localStorage.setItem(
-                "isLoggedIn",
-                "true"
-            );
-
-            localStorage.setItem(
-                "user",
-                JSON.stringify(data.user)
-            );
-
-            localStorage.setItem(
-                "userId",
-                data.user.id
-            );
-
-            localStorage.setItem(
-                "userEmail",
-                data.user.email
-            );
-
-            // ==============================
-            // REDIRECTION
-            // ==============================
-
-            window.location.href =
-                "index.html";
-
-        } catch (error) {
-
-            console.error(
-                "Erreur connexion :",
-                error
-            );
-
-            alert(
-                "Impossible de communiquer avec le serveur."
-            );
-        }
+        // Aller au Dashboard
+        window.location.href = "index.html";
     });
+
 });
