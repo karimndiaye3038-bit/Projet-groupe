c6e62fc148244864612b3f9f1efdeac921666d40
unknown <karindiaye3038@gmail.com>
2026-08-19 10:21:00 +0000

    dernier commit

--- Backend/src/models/Task.js
+++ Backend/src/models/Task.js
@@ -1,53 +1,53 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const taskSchema = new mongoose.Schema(
-//   {
-//     title: {
-//       type: String,
-//       required: true,
-//       trim: true,
-//     },
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
 
-//     description: {
-//       type: String,
-//       default: "",
-//     },
+    description: {
+      type: String,
+      default: "",
+    },
 
-//     status: {
-//       type: String,
-//       enum: ["todo", "in-progress", "paused", "completed"],
-//       default: "todo",
-//     },
+    status: {
+      type: String,
+      enum: ["todo", "in-progress", "paused", "completed"],
+      default: "todo",
+    },
 
-//     priority: {
-//       type: String,
-//       enum: ["low", "medium", "high", "urgent"],
-//       default: "medium",
-//     },
+    priority: {
+      type: String,
+      enum: ["low", "medium", "high", "urgent"],
+      default: "medium",
+    },
 
-//     project: {
-//       type: String,
-//       required: true,
-//     },
+    project: {
+      type: String,
+      required: true,
+    },
 
-//     assignedMember: {
-//       type: String,
-//       required: true,
-//     },
+    assignedMember: {
+      type: String,
+      required: true,
+    },
 
-//     tags: {
-//       type: [String],
-//       default: [],
-//     },
+    tags: {
+      type: [String],
+      default: [],
+    },
 
-//     deadline: {
-//       type: Date,
-//       required: true,
-//     },
-//   },
-//   {
-//     timestamps: true,
-//   }
-// );
+    deadline: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-// module.exports = mongoose.model("Task", taskSchema);
+module.exports = mongoose.model("Task", taskSchema);
