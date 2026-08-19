6e46deeae827e415f4bd0d6f978e7f87994f07c7
unknown <karindiaye3038@gmail.com>
2026-08-19 11:36:01 +0000

    fix TaskController import case

--- Backend/src/routes/taskRoutes.js
+++ Backend/src/routes/taskRoutes.js
@@ -6,13 +6,30 @@ const {
   getTaskById,
   updateTask,
   deleteTask,
-} = require("../controllers/taskController");
+} = require("../controllers/TaskController");
 
 const {
   validateTask,
 } = require("../middlewares/validation");
 
 const router = express.Router();
+
+// Créer une tâche
+router.post("/", validateTask, createTask);
+
+// Afficher toutes les tâches
+router.get("/", getTasks);
+
+// Afficher une tâche
+router.get("/:id", getTaskById);
+
+// Modifier une tâche
+router.put("/:id", updateTask);
+
+// Supprimer une tâche
+router.delete("/:id", deleteTask);
+
+module.exports = router;
 
 // ================================
 // CRÉER UNE TÂCHE
